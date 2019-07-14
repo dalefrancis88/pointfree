7552c5f7dd3aa9cc8abd7c08e3b87fd8306f7894
Dale Francis <dalefrancis88@gmail.com>
2019-07-14 00:19:20 -0700

    Adding simple implementation of every

---
+++ packages/js/src/array/every.js
@@ -0,0 +1,9 @@
+/** @license ISC License (c) copyright 2019 original and current authors */
+/** @author Dale Francis (dalefrancis88) */
+
+// every :: (a -> Boolean) -> Array -> Boolean
+const every = (fn, arr) => {
+  return arr.every(fn)
+}
+
+export default every
